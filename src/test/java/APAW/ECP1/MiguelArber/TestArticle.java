404b1160fe06dece0b6af1d503453cc2d9cfc4d3
MiguelArber <miguel.arber@gmail.com>
2017-10-05 11:20:24 +0200

    Added tests for ShoppingCart and Article classes

--- src/test/java/APAW/ECP1/MiguelArber/TestArticle.java
+++ src/test/java/APAW/ECP1/MiguelArber/TestArticle.java
@@ -1,5 +1,43 @@
 package APAW.ECP1.MiguelArber;
 
-public class TestArticle {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import APAW.ECP1.MiguelArber.Article;
+
+public class TestArticle {
+	
+	private Article article;
+	
+	@Before
+	public void before() {
+		
+		article = new Article(1, "ref#123"); //Constructor 1
+		
+	}
+	
+	@Test
+	public void testID() {
+		
+		assertEquals(1, article.getID());
+		
+	}
+	
+	@Test
+	public void testReference() {
+		
+		assertEquals("ref#123", article.getReference());
+		
+	}
+	
+	@Test
+	public void testStock() {
+		
+		assertEquals(0, article.getStock());
+		article.setStock(10);
+		assertEquals(10, article.getStock());
+		
+	}
 }
--- src/test/java/APAW/ECP1/MiguelArber/TestShoppingCart.java
+++ src/test/java/APAW/ECP1/MiguelArber/TestShoppingCart.java
@@ -1,5 +1,65 @@
 package APAW.ECP1.MiguelArber;
 
-public class TestShoppingCart {
+import static org.junit.Assert.assertEquals;
 
+import java.util.Calendar;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import APAW.ECP1.MiguelArber.ShoppingCart;
+
+public class TestShoppingCart {
+	
+	private ShoppingCart cart;
+	
+	@Before
+	public void before() {
+		
+		cart = new ShoppingCart(1, "Carrito", false); //Constructor 1
+		
+	}
+	
+	@Test
+	public void testID() {
+		
+		assertEquals(1, cart.getID());
+		
+	}
+	
+	@Test
+	public void testName() {
+		
+		assertEquals("Carrito", cart.getName());
+		cart.setName("Cart");
+		assertEquals("Cart", cart.getName());
+		
+	}
+	
+	@Test
+	public void testUrgent() {
+		
+		assertEquals(false, cart.isUrgent());
+		
+	}
+
+	@Test
+	public void testDate() {
+		
+		Calendar date = Calendar.getInstance();
+		assertEquals(true, cart.getDate().before(date));
+		
+	}
+	
+	@Test
+	public void testArticles() {
+		
+		assertEquals(null, cart.getArticle(1));
+		Article a = new Article(1, "ref#123");
+		cart.addArticle(a);
+		assertEquals(a, cart.getArticle(1));
+		cart.removeArticle(1);
+		assertEquals(null, cart.getArticle(1));
+	}
+	
 }
