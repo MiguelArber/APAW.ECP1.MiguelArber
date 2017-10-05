8f7060d2651622e2424f0b5d7a4e20fcc92aaa08
MiguelArber <miguel.arber@gmail.com>
2017-10-05 11:19:26 +0200

    Completed the main classes ShoppingCart and Article

--- src/main/java/APAW/ECP1/MiguelArber/Article.java
+++ src/main/java/APAW/ECP1/MiguelArber/Article.java
@@ -5,6 +5,60 @@ public class Article {
 	private int id;
 	private String reference;
 	private long stock;
-	private boolean urgent;
+	private boolean discontinued;
+	
+	public Article(int id, String reference) {
+		
+		this.id = id;
+		this.reference = reference;
+		this.stock = 0;
+		this.discontinued = false;
+		
+	}
+	
+	public Article(int id, String reference, long stock, boolean discontinued) {
+		
+		this.id = id;
+		this.reference = reference;
+		this.stock = stock;
+		this.discontinued = discontinued;
+		
+	}
+	
+	public int getID() {
+		
+		return id;
+		
+	}
+	
+	public String getReference() {
+		
+		return reference;
+		
+	}
+	
+	public void setStock(long stock) {
+		
+		this.stock = stock;
+		
+	}
+	
+	public long getStock() {
+		
+		return stock;
+		
+	}
+	
+	public void setDisontinued(boolean discontinued) {
+		
+		this.discontinued = discontinued;
+		
+	}
+	
+	public boolean isDiscontinued() {
+		
+		return discontinued;
+		
+	}
 
 }
--- src/main/java/APAW/ECP1/MiguelArber/ShoppingCart.java
+++ src/main/java/APAW/ECP1/MiguelArber/ShoppingCart.java
@@ -1,6 +1,9 @@
 package APAW.ECP1.MiguelArber;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ShoppingCart {
 	
@@ -8,7 +11,87 @@ public class ShoppingCart {
 	private Calendar date;
 	private String name;
 	private boolean urgent;
-	private Article[] articles;
+	private ArrayList<Article> articles;
 	
-
+	public ShoppingCart(int id, String name, boolean urgent) {
+		
+		this.id = id;
+		this.name = name;
+		this.urgent = urgent;
+		this.date = date.getInstance();
+		this.articles = new ArrayList<Article>();
+		
+	}
+	
+	public ShoppingCart(int id, Calendar date, String name, boolean urgent, ArrayList<Article> articles) {
+		
+		this.id = id;
+		this.date = date;
+		this.name = name;
+		this.urgent = urgent;
+		this.articles = articles;
+		
+	}
+	
+	public int getID() {
+		
+		return id;
+		
+	}
+	
+	public String getName() {
+		
+		return name;
+		
+	}
+	
+	public void setName(String name) {
+		
+		this.name = name;
+		
+	}
+	
+	public boolean isUrgent() {
+		
+		return urgent;
+	}
+	
+	public Calendar getDate() {
+		
+		return date;
+		
+	}
+	
+	public Article getArticle(int id) {
+		
+		if (!articles.isEmpty()) {
+			for(int i = 0; i < articles.size(); i++) {
+				
+				if(articles.get(i).getID() == id)
+					return articles.get(i);
+			}
+		}
+		
+		return null;
+		
+	}
+	
+	public void addArticle(Article article) {
+		
+		articles.add(article);
+		
+	}
+	
+	public void removeArticle(int id)  {
+		
+		if (!articles.isEmpty()) {
+			for(int i = 0; i < articles.size(); i++) {
+						
+					if(articles.get(i).getID() == id)
+						articles.remove(i);
+			}
+		}
+		
+	}
+	
 }
