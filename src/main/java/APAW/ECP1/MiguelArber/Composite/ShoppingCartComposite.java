e2dd449fe8ad0d543acbb0944b8c661946dca07d
MiguelArber <miguel.arber@gmail.com>
2017-10-09 12:43:27 +0200

    Added new classes ShoppingCartComponent, ShoppingCartComposite and ShoppingCartLeaf, based on Composite pattern

---
+++ src/main/java/APAW/ECP1/MiguelArber/Composite/ShoppingCartComposite.java
@@ -0,0 +1,47 @@
+package APAW.ECP1.MiguelArber.Composite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShoppingCartComposite extends ShoppingCartComponent {
+	
+	List<ShoppingCartComponent> cartComponentList;
+	private String name;
+	
+	public ShoppingCartComposite (String name) {
+		
+		this.name = name;
+		cartComponentList = new ArrayList<>();
+		
+	}
+
+	@Override
+	public boolean isComposite() {
+		return true;
+	}
+
+	@Override
+	public void add(ShoppingCartComponent cartComponent) {
+		cartComponentList.add(cartComponent);
+		
+	}
+
+	@Override
+	public void remove(ShoppingCartComponent cartComponent) {
+		cartComponentList.remove(cartComponent);
+		
+	}
+	
+	public String getName() {
+		
+		return name;
+	}
+
+	@Override
+	public String view() {
+		
+		return name;
+		
+	}
+
+}
